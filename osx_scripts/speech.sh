559da977ca2874165334feba810bd2fc40f469ab
blackccpie <blackccpie@free.fr>
2017-05-29 17:19:06 +0200

    - updated osx script to use afplay.

--- osx_scripts/speech.sh
+++ osx_scripts/speech.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?tl=en&q=$*&client=t"; }
+say() { curl -s -A Mozilla "http://translate.google.com/translate_tts?tl=en&q=$*&client=t" > output.mp3; afplay output.mp3; }
 say $*
+
+# old commandline using mplayer
+# say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?tl=en&q=$*&client=t"; }
